9d4a336cb31d661ac2b3bf1fe40b273b572c2e22
Masato Shuto <154576364+ms-yozakura@users.noreply.github.com>
2026-07-09 13:37:17 +0900

    ADD: loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+import Loading from "@/shared/ui/Loading";
+import styles from "./loading.module.css"
+
+
+export default function Page() {
+  return (<div className={styles.loadingContiner}><Loading /></div>)
+}
